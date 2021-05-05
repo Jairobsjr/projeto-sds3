31d4f6d992710a2b91b5fc6ce8aa219e7e863dff
Jairo <jairo.jb@gmail.com>
2021-05-04 23:34:05 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+          <p className="text-light">App desenvolvido por <a href="https://github.com/Jairobsjr" target="_blank" rel="noreferrer">Jairo Barbosa</a></p>
+          <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+            Evento promovido pela escola DevSuperior: <a href="https://www.instagram.com/jairo_bsjr/" target="_blank" rel="noreferrer">@jairo_bsjr</a></small></p>
+        </div>
+      </footer>
+    );
+  }
+  
+  export default Footer;
