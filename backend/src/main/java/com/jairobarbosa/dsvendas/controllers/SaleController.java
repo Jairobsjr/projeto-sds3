000e3c6d176c0b8c7522938dcfc3944835ffe025
Jairo <jairo.jb@gmail.com>
2021-05-06 23:09:41 -0300

    Group by search

--- backend/src/main/java/com/jairobarbosa/dsvendas/controllers/SaleController.java
+++ backend/src/main/java/com/jairobarbosa/dsvendas/controllers/SaleController.java
@@ -1,5 +1,7 @@
 package com.jairobarbosa.dsvendas.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jairobarbosa.dsvendas.dto.SaleDTO;
+import com.jairobarbosa.dsvendas.dto.SaleSuccessDTO;
+import com.jairobarbosa.dsvendas.dto.SaleSumDTO;
 import com.jairobarbosa.dsvendas.services.SaleService;
 
 @RestController
@@ -21,6 +25,18 @@ public class SaleController {
 	@GetMapping
 	public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable) {
 		Page<SaleDTO> list = service.findAll(pageable);
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping("/amount-by-seller")
+	public ResponseEntity<List<SaleSumDTO>> amountGroupedSeller() {
+		List<SaleSumDTO> list = service.amountGroupedSeller();
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping("/success-by-seller")
+	public ResponseEntity<List<SaleSuccessDTO>> sucessGroupedSeller() {
+		List<SaleSuccessDTO> list = service.successGroupedSeller();
 		return ResponseEntity.ok(list);
 	}
 }
--- backend/src/main/java/com/jairobarbosa/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/jairobarbosa/dsvendas/services/SaleService.java
@@ -1,5 +1,7 @@
 package com.jairobarbosa.dsvendas.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.jairobarbosa.dsvendas.dto.SaleDTO;
+import com.jairobarbosa.dsvendas.dto.SaleSuccessDTO;
+import com.jairobarbosa.dsvendas.dto.SaleSumDTO;
 import com.jairobarbosa.dsvendas.entities.Sale;
 import com.jairobarbosa.dsvendas.repositories.SaleRepository;
 import com.jairobarbosa.dsvendas.repositories.SellerRepository;
@@ -25,6 +29,16 @@ public class SaleService {
 		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(entity -> new SaleDTO(entity));
+	}
+	
+	@Transactional(readOnly = true)
+	public	List<SaleSumDTO> amountGroupedSeller() {
+		return repository.amountGroupedSeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public	List<SaleSuccessDTO> successGroupedSeller() {
+		return repository.successGroupedSeller();
 	}
 
 }
